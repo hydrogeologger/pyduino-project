462e4f3e6243ad9d1012414ade867084e251c3af
Levi <fulcan19@hotmail.com>
2022-09-26 18:15:56 +1000

    shell execution script

---
+++ yarwun/yarwun_run_once.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# sleep 23
+#su pi
+cd /home/pi/pyduino-project/yarwun/
+#/usr/bin/stdbuf -i0 -o0 -e0 python yarwun_column_a.py >> yarwun_column_a.txt 2>&1
+#/usr/bin/stdbuf -i0 -o0 -e0 python yarwun_column_b.py >> yarwun_column_b.txt 2>&1
