dfa568f997faec06e3a5ddd5e2ef2fe412dc4654
Levi <fulcan19@hotmail.com>
2023-04-17 12:14:19 +1000

    Added execution bash template

---
+++ phosphate_hill/run_template.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Use this file as a template but make your own copy of it.
+# For files to be using crontab to schedule to run at specific times.
+# You do not require sleep.
+
+
+# sleep 23 # Only required if script is to be executed at boot time
+#su pi
+
+PROJECT_DIR=/home/pi/pyduino-project/phosphate_hill
+
+cd "$PROJECT_DIR" || { echo "Unable to enter $PROJECT_DIR"; exit 2; }
+
+# Run the python in unbuffered mode
+/usr/bin/stdbuf -i0 -oL -eL python -u station-#.py >> log_phosphatehill-#.log 2>&1
