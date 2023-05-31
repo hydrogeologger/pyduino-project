e9b08a19fb5e972e70a29ad44196ef4ca99e9e02
Levi <fulcan19@hotmail.com>
2023-05-31 13:39:36 +1000

    Added extra suction sensors
    * Remove default data field values
    * Added extra data field names for suction sensors
    * Transfer oxygen sensor power to RPI switches
    * Fixed battery reported in ADC rather than voltage
    * Fixed voltage precision

--- phosphate_hill/station-1-weather.py
+++ phosphate_hill/station-1-weather.py
@@ -16,7 +16,7 @@ import mqtthelper # MQTT helper module for publishing archive
 This is the code for the Phosphate Hill Rehabilitation Cover Project.
 Weather Station
 The peripherals include:
-    1x Camera
+    Up to 2x Camera
     1x UV Sensor
     1x Wind Anemometer
     1x Rain Gauge (Tip Bucket)
@@ -24,7 +24,7 @@ The peripherals include:
     2x Strings of Sensors per Station.
         Per String:
         4x Teros 12 Moisture Sensors
-        4x Suction Sensors
+        6x Suction Sensors
         2x Oxygen Sensors
 """
 #------------------- Constants and Ports Information---------------------------
@@ -54,14 +54,14 @@ field_name = ["battery",
             'mos1','mos2','mos3','mos4','mos5','mos6','mos7','mos8',
             'mos1_temp','mos2_temp','mos3_temp','mos4_temp','mos5_temp','mos6_temp','mos7_temp','mos8_temp',
             'mos1_ec','mos2_ec','mos3_ec','mos4_ec','mos5_ec','mos6_ec','mos7_ec','mos8_ec',
-            'suct1','suct2','suct3','suct4','suct5','suct6','suct7','suct8',
+            'suct1','suct2','suct3','suct4','suct5','suct6','suct7','suct8','suct9', 'suct10', 'suct11', 'suct12',
             'oxy1','oxy2','oxy3','oxy4',
             'oxy1_temp','oxy2_temp','oxy3_temp','oxy4_temp',
             'oxy1_baro','oxy2_baro','oxy3_baro','oxy4_baro',
             'oxy1_percent','oxy2_percent','oxy3_percent','oxy4_percent'
             ]
 
-data_collected = dict((el,0.0) for el in field_name)
+data_collected = {}
 
 #---------------------------- Initiation --------------------------------------
 if (PUBLISH_TO_THINGSBOARD):
@@ -73,8 +73,8 @@ if (PUBLISH_TO_THINGSBOARD):
         # accurately tracked.
         # Please use unique client_id for each client, and you should be able to use
         # same thingsboard device token for multiple clients.
-        client = mqtt.Client(client_id="phosphatehill_#")
-        client.username_pw_set(credential['access_token_phosphate_hill'])
+        client = mqtt.Client(client_id="phosphatehill_1")
+        client.username_pw_set(credential['access_token_phosphatehill_1'])
         client.connect(credential['thingsboard_host'], 1883, 60)
         client.loop_start()
     except Exception:
@@ -98,8 +98,9 @@ try:
             csv_fid = open(CSV_FILE_NAME, 'w', buffering=1) # Open file for writing
             # TODO: Check headers
             csv_fid.write("Timestamp, Temp_Internal (C),Humidity_Internal (%)," \
-                        "Light_Vis,Light_IR,Light_UV," \
                         "Temp_Ambient (C), Humidity_Ambient (%)," \
+                        "Wind_Direction (Deg)," \
+                        "Light_Vis, Light_IR, Light_UV (index*100)," \
                         "MOS1_VWP, MOS1_Temp, MOS1_EC," \
                         "MOS2_VWP, MOS2_Temp, MOS2_EC," \
                         "MOS3_VWP, MOS3_Temp, MOS3_EC," \
@@ -109,6 +110,7 @@ try:
                         "MOS7_VWP, MOS7_Temp, MOS7_EC," \
                         "MOS8_VWP, MOS8_Temp, MOS8_EC," \
                         "SUCT1 (C),SUCT2 (C),SUCT3 (C),SUCT4 (C),SUCT5 (C),SUCT6 (C),SUCT7 (C),SUCT8 (C)," \
+                        "SUCT9 (C),SUCT10 (C),SUCT11 (C),SUCT12 (C)," \
                         "OXY1,OXY1_Temp,OXY1_Baro,OXY1_%," \
                         "OXY2,OXY2_Temp,OXY2_Baro,OXY2_%," \
                         "OXY3,OXY3_Temp,OXY3_Baro,OXY3_%," \
@@ -132,22 +134,22 @@ try:
         msg = ard.readline().decode()
         current_read = msg.split(',')[0:-1]
         battery_adc = float(current_read[-1])
-        # ADC * (ADC_Resolution) * ((R2 + R3) / R3)
-        vref = 5.1 # ADC Reference Voltage
-        r2 = 120e3
-        r3 = 39e3
-        battery_voltage = battery_adc * float(((r2 + r3)/r3)) * float((vref/1024))
-
     except Exception as error:
         if SCREEN_DISPLAY:
             print(msg.strip())
             print("Battery Reading failed, error: {0}".format(sys.exc_info()[0]))
             # print("Battery Reading failed, error:", type(error), error)
     else:
+        # ADC * (ADC_Resolution) * ((R2 + R3) / R3)
+        vref = 5.1 # ADC Reference Voltage
+        r2 = 120e3
+        r3 = 39e3
+        battery_voltage = battery_adc * float(((r2 + r3)/r3)) * float((vref/1024))
+        battery_voltage = round(battery_voltage, 2)
         #data_collected['battery_adc'] = battery_adc
         data_collected['battery_voltage'] = battery_voltage
         if SCREEN_DISPLAY:
-            print("Battery Voltage (ADC): " + str(battery_adc) + ", " + str(battery_voltage) + "V")
+            print("Battery Voltage (ADC): " + str(battery_adc) + ", " + str(battery_voltage) + " V")
         if SAVE_TO_CSV:
             # csv_fid.write(time_now_local_str + DELIMITER + 'Battery Voltage' + DELIMITER + "ADC" + DELIMITER+DELIMITER + str(battery_adc) + '\r\n')
             # csv_fid.write(time_now_local_str + DELIMITER + 'Battery Voltage' + DELIMITER + "V" + DELIMITER+DELIMITER + str(battery_voltage) + '\r\n')
@@ -193,7 +195,6 @@ try:
         ard.write(command.encode())
         ard.flushInput()
         msg = ard.readline().decode() # <temperature>,humidity,
-        GPIO.output(rpi_pin, GPIO.LOW)
         current_read = msg.split(',')[0:-1]
         sht31_temperature = float(current_read[-2])
         sht31_humidity = float(current_read[-1])
@@ -211,13 +212,13 @@ try:
         if SAVE_TO_CSV:
             csv_fid.write("{0}{1}{0}{2}".format(DELIMITER, sht31_temperature, sht31_humidity))
     finally:
-        pass
+        GPIO.output(rpi_pin, GPIO.LOW)
 
     # ------------------------- Wind Direction --------------------------------
     # Wind Direction
     try:
         sensor_name = "wind_dir"
-        wind_dir_device = WindDirection(name=sensor_name, offset=0)
+        wind_dir_device = WindDirection(name=sensor_name, offset=270)
         GPIO.setmode(GPIO.BCM)
         rpi_pin = 6
         GPIO.setup(rpi_pin, GPIO.OUT)
@@ -226,7 +227,6 @@ try:
         ard.write(command.encode())
         ard.flushInput()
         msg = ard.readline().decode() # <adc>,
-        GPIO.output(rpi_pin, GPIO.LOW)
         current_read = msg.split(',')[0:-1]
         wind_direction_adc = float(current_read[-1])
     except Exception:
@@ -236,14 +236,14 @@ try:
         if SAVE_TO_CSV:
             csv_fid.write(DELIMITER)
     else:
-        wind_direction_avg = wind_dir_device.get_calibrated_direction(wind_direction_adc)
-        data_collected['wind_dir_avg'] = wind_direction_avg
+        wind_direction_degree = wind_dir_device.get_calibrated_direction(wind_direction_adc)
+        data_collected['wind_dir'] = wind_direction_degree
         if SCREEN_DISPLAY:
             print(sensor_name.upper() + ": " + msg.rstrip())
         if SAVE_TO_CSV:
-            csv_fid.write("{0}{1}".format(DELIMITER, wind_direction_avg))
+            csv_fid.write("{0}{1}".format(DELIMITER, wind_direction_degree))
     finally:
-        pass
+        GPIO.output(rpi_pin, GPIO.LOW)
 
     # --------------------------------- UV & Iradiance-----------------------------
     # UV and Iradiance Sensor
@@ -257,7 +257,6 @@ try:
         ard.write(command.encode())
         ard.flushInput()
         msg = ard.readline().decode() # Vis,261.67,IR,264.00,UV,2.33,
-        GPIO.output(rpi_pin, GPIO.LOW)
         current_read = msg.split(',')[0:-1]
         light_visible = float(current_read[-5])
         light_ir = float(current_read[-3])
@@ -277,7 +276,7 @@ try:
         if SAVE_TO_CSV:
             csv_fid.write("{0}{1}{0}{2}{0}{3}".format(DELIMITER, light_visible, light_ir, light_uv))
     finally:
-        pass
+        GPIO.output(rpi_pin, GPIO.LOW)
 
     #---------------------------Moisture Sensor 1--------------
     # Moisture Sensor 1
@@ -720,12 +719,120 @@ try:
             csv_fid.write(DELIMITER + str(temp_diff))
             # csv_fid.write(time_now_local_str + DELIMITER + field_name[field_name_id] + DELIMITER + msg.rstrip() + '\r\n')
 
+    #---------------------------Suction Sensor 9--------------------------------
+    # Suction Sensor 9
+    try:
+        sensor_name = "suct9"
+        command = "fred,___,dgin,3,snpw,8,htpw,46,itv,6000,otno,5,debug,0"
+        ard.write(command.encode())
+        ard.flushInput()
+        msg = ard.readline().decode() #<addr>,<base temp>,<heat temp_0>,<heat temp_...>,<cool temp_0>,<cool temp_...>
+        current_read=msg.split(',')[0:-1]
+        temp_baseline = float(current_read[-11])
+        temp_diff = float(current_read[-6]) - temp_baseline
+    except Exception:
+        if SCREEN_DISPLAY:
+            print(msg.rstrip())
+            # print("{1} reading failed, error: {0}".format(sys.exc_info()[0], sensor_name.upper()))
+            print(sensor_name.upper() + " reading failed, error: " + str(sys.exc_info()[0]))
+        if SAVE_TO_CSV:
+            csv_fid.write(DELIMITER)
+    else:
+        data_collected['suct9'] = temp_diff
+        if SCREEN_DISPLAY:
+            print(sensor_name.upper() + ": " + msg.rstrip())
+        if SAVE_TO_CSV:
+            csv_fid.write(DELIMITER + str(temp_diff))
+            # csv_fid.write(time_now_local_str + DELIMITER + field_name[field_name_id] + DELIMITER + msg.rstrip() + '\r\n')
+
+    #---------------------------Suction Sensor 10--------------------------------
+    # Suction Sensor 10
+    try:
+        sensor_name = "suct10"
+        command = "fred,___,dgin,4,snpw,8,htpw,47,itv,6000,otno,5,debug,0"
+        ard.write(command.encode())
+        ard.flushInput()
+        msg = ard.readline().decode() #<addr>,<base temp>,<heat temp_0>,<heat temp_...>,<cool temp_0>,<cool temp_...>
+        current_read=msg.split(',')[0:-1]
+        temp_baseline = float(current_read[-11])
+        temp_diff = float(current_read[-6]) - temp_baseline
+    except Exception:
+        if SCREEN_DISPLAY:
+            print(msg.rstrip())
+            # print("{1} reading failed, error: {0}".format(sys.exc_info()[0], sensor_name.upper()))
+            print(sensor_name.upper() + " reading failed, error: " + str(sys.exc_info()[0]))
+        if SAVE_TO_CSV:
+            csv_fid.write(DELIMITER)
+    else:
+        data_collected['suct10'] = temp_diff
+        if SCREEN_DISPLAY:
+            print(sensor_name.upper() + ": " + msg.rstrip())
+        if SAVE_TO_CSV:
+            csv_fid.write(DELIMITER + str(temp_diff))
+            # csv_fid.write(time_now_local_str + DELIMITER + field_name[field_name_id] + DELIMITER + msg.rstrip() + '\r\n')
+
+    #---------------------------Suction Sensor 11--------------------------------
+    # Suction Sensor 11
+    try:
+        sensor_name = "suct11"
+        command = "fred,___,dgin,5,snpw,8,htpw,48,itv,6000,otno,5,debug,0"
+        ard.write(command.encode())
+        ard.flushInput()
+        msg = ard.readline().decode() #<addr>,<base temp>,<heat temp_0>,<heat temp_...>,<cool temp_0>,<cool temp_...>
+        current_read=msg.split(',')[0:-1]
+        temp_baseline = float(current_read[-11])
+        temp_diff = float(current_read[-6]) - temp_baseline
+    except Exception:
+        if SCREEN_DISPLAY:
+            print(msg.rstrip())
+            # print("{1} reading failed, error: {0}".format(sys.exc_info()[0], sensor_name.upper()))
+            print(sensor_name.upper() + " reading failed, error: " + str(sys.exc_info()[0]))
+        if SAVE_TO_CSV:
+            csv_fid.write(DELIMITER)
+    else:
+        data_collected['suct11'] = temp_diff
+        if SCREEN_DISPLAY:
+            print(sensor_name.upper() + ": " + msg.rstrip())
+        if SAVE_TO_CSV:
+            csv_fid.write(DELIMITER + str(temp_diff))
+            # csv_fid.write(time_now_local_str + DELIMITER + field_name[field_name_id] + DELIMITER + msg.rstrip() + '\r\n')
+
+    #---------------------------Suction Sensor 12--------------------------------
+    # Suction Sensor 12
+    try:
+        sensor_name = "suct12"
+        command = "fred,___,dgin,64,snpw,8,htpw,49,itv,6000,otno,5,debug,0"
+        ard.write(command.encode())
+        ard.flushInput()
+        msg = ard.readline().decode() #<addr>,<base temp>,<heat temp_0>,<heat temp_...>,<cool temp_0>,<cool temp_...>
+        current_read=msg.split(',')[0:-1]
+        temp_baseline = float(current_read[-11])
+        temp_diff = float(current_read[-6]) - temp_baseline
+    except Exception:
+        if SCREEN_DISPLAY:
+            print(msg.rstrip())
+            # print("{1} reading failed, error: {0}".format(sys.exc_info()[0], sensor_name.upper()))
+            print(sensor_name.upper() + " reading failed, error: " + str(sys.exc_info()[0]))
+        if SAVE_TO_CSV:
+            csv_fid.write(DELIMITER)
+    else:
+        data_collected['suct12'] = temp_diff
+        if SCREEN_DISPLAY:
+            print(sensor_name.upper() + ": " + msg.rstrip())
+        if SAVE_TO_CSV:
+            csv_fid.write(DELIMITER + str(temp_diff))
+            # csv_fid.write(time_now_local_str + DELIMITER + field_name[field_name_id] + DELIMITER + msg.rstrip() + '\r\n')
+
 
     #---------------------------Oxygen Sensor 1--------------------------------
     # Oxygen Sensor 1
     try:
         sensor_name = "oxy1"
-        command = "luminox,A,power,46,serial,1,debug,0"
+        GPIO.setmode(GPIO.BCM)
+        rpi_pin = 23
+        GPIO.setup(rpi_pin, GPIO.OUT)
+        GPIO.output(rpi_pin, GPIO.HIGH)
+        command = "luminox,A,power,-1,serial,1,debug,0"
         ard.write(command.encode())
         ard.flushInput()
         msg = ard.readline().decode() # “O xxxx.x T yxx.x P xxxx % xxx.xx e xxxx\r\n”
@@ -751,12 +858,18 @@ try:
         if SAVE_TO_CSV:
             csv_fid.write(DELIMITER + str(o2_level) + DELIMITER + str(o2_temp) + DELIMITER + str(o2_baro) + DELIMITER + str(o2_percent))
             # csv_fid.write(time_now_local_str + DELIMITER + field_name[field_name_id] + DELIMITER + msg.rstrip() + '\r\n')
+    finally:
+        GPIO.output(rpi_pin, GPIO.LOW)
 
     #---------------------------Oxygen Sensor 2--------------------------------
     # Oxygen Sensor 2
     try:
         sensor_name = "oxy2"
-        command = "luminox,A,power,47,serial,3,debug,0"
+        GPIO.setmode(GPIO.BCM)
+        rpi_pin = 24
+        GPIO.setup(rpi_pin, GPIO.OUT)
+        GPIO.output(rpi_pin, GPIO.HIGH)
+        command = "luminox,A,power,-1,serial,3,debug,0"
         ard.write(command.encode())
         ard.flushInput()
         msg = ard.readline().decode() # “O xxxx.x T yxx.x P xxxx % xxx.xx e xxxx\r\n”
@@ -782,12 +895,18 @@ try:
         if SAVE_TO_CSV:
             csv_fid.write(DELIMITER + str(o2_level) + DELIMITER + str(o2_temp) + DELIMITER + str(o2_baro) + DELIMITER + str(o2_percent))
             # csv_fid.write(time_now_local_str + DELIMITER + field_name[field_name_id] + DELIMITER + msg.rstrip() + '\r\n')
+    finally:
+        GPIO.output(rpi_pin, GPIO.LOW)
 
     #---------------------------Oxygen Sensor 3--------------------------------
     # Oxygen Sensor 3
     try:
         sensor_name = "oxy3"
-        command = "luminox,A,power,48,serial,2,debug,0"
+        GPIO.setmode(GPIO.BCM)
+        rpi_pin = 25
+        GPIO.setup(rpi_pin, GPIO.OUT)
+        GPIO.output(rpi_pin, GPIO.HIGH)
+        command = "luminox,A,power,-1,serial,2,debug,0"
         ard.write(command.encode())
         ard.flushInput()
         msg = ard.readline().decode() # “O xxxx.x T yxx.x P xxxx % xxx.xx e xxxx\r\n”
@@ -813,12 +932,18 @@ try:
         if SAVE_TO_CSV:
             csv_fid.write(DELIMITER + str(o2_level) + DELIMITER + str(o2_temp) + DELIMITER + str(o2_baro) + DELIMITER + str(o2_percent))
             # csv_fid.write(time_now_local_str + DELIMITER + field_name[field_name_id] + DELIMITER + msg.rstrip() + '\r\n')
+    finally:
+        GPIO.output(rpi_pin, GPIO.LOW)
 
     #---------------------------Oxygen Sensor 4--------------------------------
     # Oxygen Sensor 4
     try:
         sensor_name = "oxy4"
-        command = "luminox,A,power,49,serial,3,debug,0"
+        GPIO.setmode(GPIO.BCM)
+        rpi_pin = 26
+        GPIO.setup(rpi_pin, GPIO.OUT)
+        GPIO.output(rpi_pin, GPIO.HIGH)
+        command = "luminox,A,power,-1,serial,3,debug,0"
         ard.write(command.encode())
         ard.flushInput()
         msg = ard.readline().decode() # “O xxxx.x T yxx.x P xxxx % xxx.xx e xxxx\r\n”
@@ -844,6 +969,8 @@ try:
         if SAVE_TO_CSV:
             csv_fid.write(DELIMITER + str(o2_level) + DELIMITER + str(o2_temp) + DELIMITER + str(o2_baro) + DELIMITER + str(o2_percent))
             # csv_fid.write(time_now_local_str + DELIMITER + field_name[field_name_id] + DELIMITER + msg.rstrip() + '\r\n')
+    finally:
+        GPIO.output(rpi_pin, GPIO.LOW)
 
 
     #----------------------------Upload data -----------------------------------
@@ -889,4 +1016,3 @@ finally:
     if (PUBLISH_TO_THINGSBOARD and client.is_connected()):
         client.loop_stop()
         client.disconnect()
-
